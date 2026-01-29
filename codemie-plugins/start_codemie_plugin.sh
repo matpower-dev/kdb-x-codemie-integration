257f74d91a6cf0afd35eaec7f33f346f1cd90187
Matthew Power <mjmpower06@gmail.com>
2026-01-29 15:23:54 +0000

    2025.01.29-1

--- codemie-plugins/start_codemie_plugin.sh
+++ codemie-plugins/start_codemie_plugin.sh
@@ -4,5 +4,22 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Change into that directory
 cd "$SCRIPT_DIR" || exit 1
+
+mkdir -p "$HOME/.codemie"
+cat <<EOF > "$HOME/.codemie/config.json"
+{
+  "PLUGIN_KEY": "ae4be9da-aa1f-47cb-85c8-2764d37271e1",
+  "mcpServers": {
+    "kdbx-streamable": {
+      "command": "npx",
+      "args": [
+         "mcp-remote",
+         "http://localhost:8000/mcp"
+      ]
+    }
+  }
+}
+EOF
+
 echo "hello world"
 codemie-plugins mcp run -s kdbx-streamable
